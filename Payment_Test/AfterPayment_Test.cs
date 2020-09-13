c7e2991b205c63e4dd6338635e57feac2d9f4fab
Shrikant <shrikanttalmale@yahoo.co.in>
2020-09-13 15:05:53 +0530

    Commit from VS

---
+++ Payment_Test/AfterPayment_Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Payment_Test
+{
+    [TestClass]
+    public class AfterPayment_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
